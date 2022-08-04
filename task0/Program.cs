f193f3f714e8afcb4658c723c8e7e67585e14809
Viktor Belous <vyntyk@yandex.ru>
2022-08-04 20:38:35 +0300

    Конкатенация

---
+++ task0/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int userNumber = new int();
+        Console.WriteLine("Введите число: ");
+        userNumber = Convert.ToInt32(Console.ReadLine());
+
+        int result = (userNumber * userNumber);
+
+        Console.WriteLine("Степень числа " + userNumber + " = " + result);
+    }
+}
