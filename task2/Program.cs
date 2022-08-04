5f4fab7f69f78d385f77ff9b6e58e276a596148e
Viktor Belous <vyntyk@yandex.ru>
2022-08-04 22:15:41 +0300

    Resheniya

---
+++ task2/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число: ");
+int day = Convert.ToInt32(Console.ReadLine());
+if(day <=0 || day>7) Console.WriteLine("Неправильное число");
+else if(day == 1) Console.WriteLine("Понедельник");
+else if(day == 2) Console.WriteLine("Вторник");
+else if(day == 3) Console.WriteLine("Среда");
+else if(day == 4) Console.WriteLine("Четверг");
+else if(day == 5) Console.WriteLine("Пятница");
+else if(day == 6) Console.WriteLine("Суббота");
+else if(day == 7) Console.WriteLine("Воскресенье");
---
+++ task4/Program.cs
@@ -0,0 +1,8 @@
+﻿
+Console.WriteLine("Введите трехзначное число: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+int number2 = number%10;
+
+Console.WriteLine(number2);
